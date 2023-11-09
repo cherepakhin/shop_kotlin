bf61fb4823275ecdf05cdb2bc82ace79bc7f3f51
Черепахин Василий <vasi.che@gmail.com>
2023-11-09 19:06:51 +0500

    add UUIDTest.kt

---
+++ src/test/kotlin/ru/perm/v/shopkotlin/other/UUIDTest.kt
@@ -0,0 +1,13 @@
+package ru.perm.v.shopkotlin.other
+
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertFalse
+
+internal class UUIDTest {
+    @Test
+    internal fun create() {
+        val uuid = UUID.randomUUID().toString()
+        assertFalse(uuid.isBlank())
+    }
+}
