20e20fe20a9057c2fd38c789afe3c504ce421e20
Черепахин Василий <vasi.che@gmail.com>
2023-11-11 01:57:08 +0500

    add test Pair_mapOf_to_Test.kt

--- src/test/kotlin/ru/perm/v/shopkotlin/other/Pair_mapOf_to_Test.kt
+++ src/test/kotlin/ru/perm/v/shopkotlin/other/Pair_mapOf_to_Test.kt
@@ -19,4 +19,35 @@ class Pair_mapOf_to_Test {
         assertEquals(2, mapPair.size)
         assertEquals("100", mapPair.get(10))
     }
+
+    @Test
+    internal fun muttableListTest() {
+        val list = mutableListOf<Int>()
+        list.add(200)
+        assertEquals(1, list.size)
+        assertEquals(200, list.get(0))
+    }
+
+    @Test
+    internal fun unmuttableListTest() {
+        val list = listOf<Int>(100)
+//        list.add(200) -> ERR, unmuttable
+        assertEquals(1, list.size)
+        assertEquals(100, list.get(0))
+    }
+
+    @Test
+    internal fun unmattableMapTest() {
+        var map = mapOf(10 to 100, 20 to 200)
+        assertEquals(2, map.size)
+        assertEquals(100, map.get(10))
+        assertEquals(200, map.get(20))
+
+        map = map.plus(20 to 300)
+        map = map.plus(40 to 400)
+        assertEquals(3, map.size)
+        assertEquals(100, map.get(10))
+        assertEquals(300, map.get(20))
+        assertEquals(400, map.get(40))
+    }
 }
