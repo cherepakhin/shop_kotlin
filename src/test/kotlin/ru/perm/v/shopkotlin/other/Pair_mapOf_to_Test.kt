f72636d53274f7d28409e7e0b8380b172ed93bd8
Черепахин Василий <vasi.che@gmail.com>
2023-11-11 01:33:54 +0500

    add test work cache in ProductRestCacheTest.kt

--- src/test/kotlin/ru/perm/v/shopkotlin/other/Pair_mapOf_to_Test.kt
+++ src/test/kotlin/ru/perm/v/shopkotlin/other/Pair_mapOf_to_Test.kt
@@ -6,15 +6,15 @@ import kotlin.test.assertEquals
 class Pair_mapOf_to_Test {
     @Test
     internal fun mapTo() {
-        val mapSimple = mapOf<Int, String>(10 to "100", 20 to "200")
+        val mapSimple = mapOf(10 to "100", 20 to "200")
         assertEquals(2, mapSimple.size)
         assertEquals("100", mapSimple.get(10))
     }
 
     @Test
     internal fun pair() {
-        val pair1 = Pair<Int, String>(10, "100")
-        val pair2 = Pair<Int, String>(20, "200")
+        val pair1 = Pair(10, "100")
+        val pair2 = Pair(20, "200")
         val mapPair = mapOf(pair1, pair2)
         assertEquals(2, mapPair.size)
         assertEquals("100", mapPair.get(10))
