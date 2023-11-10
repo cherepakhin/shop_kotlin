6981107ed3a5e8f2eefa51065b701b3b5ba04f1d
Черепахин Василий <vasi.che@gmail.com>
2023-11-11 01:33:19 +0500

    add test Pair_mapOf_to_Test

---
+++ src/test/kotlin/ru/perm/v/shopkotlin/other/Pair_mapOf_to_Test.kt
@@ -0,0 +1,22 @@
+package ru.perm.v.shopkotlin.other
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Pair_mapOf_to_Test {
+    @Test
+    internal fun mapTo() {
+        val mapSimple = mapOf<Int, String>(10 to "100", 20 to "200")
+        assertEquals(2, mapSimple.size)
+        assertEquals("100", mapSimple.get(10))
+    }
+
+    @Test
+    internal fun pair() {
+        val pair1 = Pair<Int, String>(10, "100")
+        val pair2 = Pair<Int, String>(20, "200")
+        val mapPair = mapOf(pair1, pair2)
+        assertEquals(2, mapPair.size)
+        assertEquals("100", mapPair.get(10))
+    }
+}
