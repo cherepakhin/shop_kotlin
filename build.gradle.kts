8097c0d8c347e32bf1559c689bf45a049574102b
Черепахин Василий <vasi.che@gmail.com>
2023-12-12 12:43:27 +0500

    version 0.1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "ru.perm.v"
 // change to publishing on change version
-version = "0.1.18"
+version = "0.1.19"
 description = "shop_kotlin"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
