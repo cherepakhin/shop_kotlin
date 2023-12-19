7b488d7a4b4808f85ecda188847b60894e5d3fcb
Черепахин Василий <vasi.che@gmail.com>
2023-12-19 11:37:14 +0500

    remove "open" from ProductEntity.kt

--- src/main/kotlin/ru/perm/v/shopkotlin/entity/ProductEntity.kt
+++ src/main/kotlin/ru/perm/v/shopkotlin/entity/ProductEntity.kt
@@ -4,18 +4,18 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "product")
-open class ProductEntity {
+class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // hibernate sequence by next max
     @Column(name = "n", nullable = false)
     // column name must is not "id", "id" is key word
-    open var n: Long = -1L
+    var n: Long = -1L
 
     @Column(name = "name", nullable = false)
-    open var name: String = ""
+    var name: String = ""
 
     @Column(name = "group_product_n", nullable = false)
-    open var groupProductN: Long = -1
+    var groupProductN: Long = -1
 
     constructor(n: Long, name: String, groupProductN: Long) {
         this.n = n
