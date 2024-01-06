0774d0b551996f0170c4171e511de2f41e11f826
Черепахин Василий <vasi.che@gmail.com>
2024-01-06 22:50:52 +0500

    add to docker_build.sh "...bootBuildImage..." etc

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,10 @@
-./gradlew build
-docker build --build-arg JAR_FILE=build/libs/shop_kotlin-0.1.11.jar -t shop_kotlin/app .
+#./gradlew build
+# size=52914
+
+./gradlew bootBuildImage
+# size=56783818
+# BUILD SUCCESSFUL in 3m 8s
+
+docker build --build-arg JAR_FILE=build/libs/shop_kotlin-0.24.0105.jar -t shop_kotlin/app .
+docker run -p 8980:8980 -p 8988:8988  docker.io/library/shop_kotlin:0.24.0105
+http :8980/shop_kotlin/api/echo/aaaa
