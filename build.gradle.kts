b3365ba71e9f45cb9eb3cc0665ec8d9578437b23
Черепахин Василий <vasi.che@gmail.com>
2024-01-08 18:30:41 +0500

    version = "0.24.0108"

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "ru.perm.v"
 // change version on publishing
-version = "0.24.0105"
+version = "0.24.0108"
 description = "shop kotlin description"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
