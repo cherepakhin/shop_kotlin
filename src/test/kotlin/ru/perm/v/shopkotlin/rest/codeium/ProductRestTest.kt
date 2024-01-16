d9a056ab40e81ea5c819e53cbc3f4f11b93cd2e4
Черепахин Василий <vasi.che@gmail.com>
2024-01-16 22:20:37 +0500

    add test ru.perm.v.shopkotlin.rest.codeium.ProductRestTest.update()

--- src/test/kotlin/ru/perm/v/shopkotlin/rest/codeium/ProductRestTest.kt
+++ src/test/kotlin/ru/perm/v/shopkotlin/rest/codeium/ProductRestTest.kt
@@ -2,14 +2,17 @@ package ru.perm.v.shopkotlin.rest.codeium
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import ru.perm.v.shopkotlin.dto.ProductDTO
 import ru.perm.v.shopkotlin.rest.ProductRest
 import ru.perm.v.shopkotlin.service.ProductService
+import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
 class ProductRestTest {
@@ -52,5 +55,30 @@ class ProductRestTest {
         productRest.deleteById(ID)
 
         Mockito.verify(mockProductService, times(1)).delete(ID)
+    }
+
+    @Test
+    fun validate() {
+        val mockProductService = mock<ProductService>()
+        val productRest = ProductRest(mockProductService)
+        val productDTO = ProductDTO(-1L, "", -1L)
+        val exception = assertThrows<Exception> { productRest.validate(productDTO) }
+        assertEquals(
+            "ProductDTO(n=-1, name='', groupDtoN=-1) has errors: Field 'name' in ProductDTO is empty\n",
+            exception.message
+        )
+    }
+
+    @Test
+    fun update() {
+        val mockProductService = mock<ProductService>()
+        val productRest = ProductRest(mockProductService)
+        val productDTO = ProductDTO(100L, "NEW NAME", 10L)
+
+        doReturn(ProductDTO(100L, "NEW NAME", 10L)).`when`(mockProductService).update(productDTO)
+
+        val returnedProductDTO = productRest.update(-1, productDTO)
+
+        assertEquals(ProductDTO(100L, "NEW NAME", 10L), returnedProductDTO)
     }
 }
