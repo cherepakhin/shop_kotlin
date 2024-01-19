efa72360ca9b052fab459f9c34512a08f98982b9
Черепахин Василий <vasi.che@gmail.com>
2024-01-19 18:27:04 +0500

    add comment: Фильтр для групп с указанием сортировки выборки

--- src/main/kotlin/ru/perm/v/shopkotlin/filter/GroupProductFilter.kt
+++ src/main/kotlin/ru/perm/v/shopkotlin/filter/GroupProductFilter.kt
@@ -1,5 +1,8 @@
 package ru.perm.v.shopkotlin.filter
 
+/**
+ * Фильтр для групп с указанием сортировки выборки
+ */
 data class GroupProductFilter(
     val listN: List<Long>,
     val name: String = "",
