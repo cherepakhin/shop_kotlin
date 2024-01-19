c04adec02f23c43becb9def73556569fb13c28ba
Черепахин Василий <vasi.che@gmail.com>
2024-01-19 18:36:10 +0500

    add commentS for fields GroupProductFilter.kt

--- src/main/kotlin/ru/perm/v/shopkotlin/filter/GroupProductFilter.kt
+++ src/main/kotlin/ru/perm/v/shopkotlin/filter/GroupProductFilter.kt
@@ -4,7 +4,7 @@ package ru.perm.v.shopkotlin.filter
  * Filter for GroupProduct with listN, name and listSortBy
  */
 data class GroupProductFilter(
-    val listN: List<Long>,
-    val name: String = "",
-    var listSortBy: List<String> = listOf("name")
+    val listN: List<Long>, // list of GroupProduct N
+    val name: String = "", // name of GroupProduct
+    var listSortBy: List<String> = listOf("name") // list of sort fields
 )
