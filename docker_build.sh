f71127ee679ee409de7de35acc08fab6366c9ccd
Черепахин Василий <vasi.che@gmail.com>
2024-09-25 17:56:44 +0500

    add comments # run docker image (8980 - main port, 8988 - actuator) in docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -9,7 +9,7 @@
 # build docker image
 docker build --build-arg JAR_FILE=build/libs/shop_kotlin-0.24.0105.jar -t shop_kotlin/app .
 
-# run docker image
+# run docker image (8980 - main port, 8988 - actuator)
 docker run -p 8980:8980 -p 8988:8988  docker.io/library/shop_kotlin:0.24.0105
 
 # echo test
