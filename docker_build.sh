b105ec19f007d1b6373f0ef942796991c42a1023
Черепахин Василий <vasi.che@gmail.com>
2024-09-25 17:55:48 +0500

    add comments in docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,16 @@
 #./gradlew build
 # size=52914
 
+# build fat jar
 ./gradlew bootBuildImage
 # size=56783818
 # BUILD SUCCESSFUL in 3m 8s
 
+# build docker image
 docker build --build-arg JAR_FILE=build/libs/shop_kotlin-0.24.0105.jar -t shop_kotlin/app .
+
+# run docker image
 docker run -p 8980:8980 -p 8988:8988  docker.io/library/shop_kotlin:0.24.0105
+
+# echo test
 http :8980/shop_kotlin/api/echo/aaaa
